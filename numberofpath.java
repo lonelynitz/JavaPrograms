97fedfcf3a5d04c82d4a2a91a7fd18cf49f0e7fd
loneltnitzz <vasanthmurugesan47@gmail.com>
2023-07-28 20:55:45 +0530

    Java files are created

---
+++ numberofpath.java
@@ -0,0 +1,20 @@
+public class numberofpath {
+    
+    int count = 0;
+    long pathfinder(int a , int b){
+        rec(0, 0, a, b);
+        return count;
+
+    }
+     void rec(int a , int b , int m , int n){
+        if(a<0 || b<0 || a>=n || b>=m){
+            return ;
+        }
+        if(a==n-1 && b==m-1){
+            count ++;
+        }
+
+        rec(a+1, b, m, n);
+        rec(a+1, b+1, m, n);
+     }
+}
---
+++ numberpalindrome.java
@@ -0,0 +1,23 @@
+
+public class numberpalindrome{
+    
+    public static String number(int a){
+
+        String s=""+a;
+        int l=s.length();
+
+        for(int i =0;i<l/2;i++){
+            if(s.charAt(i)!= s.charAt(l-1-i)){
+                return "It is not palindrome";
+            }else{
+                return "It is palindrome";
+            }
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(number(516)) ;   
+}
+}
